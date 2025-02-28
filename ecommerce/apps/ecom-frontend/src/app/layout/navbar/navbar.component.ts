a06587b30880430b9da42ca84ab7d7681843432c
Kashish Makkar <kashishmakkar99@gmail.com>
2025-02-28 23:58:43 +0530

    navbar changes

--- ecommerce/apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
+++ ecommerce/apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-navbar',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
